9d1774e64e30924bbbe3d4a0350abdac5a89fc8d
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-10-03 14:47:28 +0300

    Impl-ed prev data loading and saving.

--- src/mon_terapevt.py
+++ src/mon_terapevt.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import time
+from os.path import dirname, abspath, join, exists
+import json
 import requests
 from mylogging import log, err
 
@@ -15,6 +17,7 @@ if MY_DEBUG:
     MON_URL = 'http://localhost:3000/db.json'
 else:
     MON_URL = 'https://sarov.r-mis.ru/pp/group/department_295/service/61/resource/161/planning/2016/10?_salt=1475064375273'
+PREV_DATA_FNAME = join(dirname(abspath(__file__)), "prev_data.json")
 
 #======================================
 
@@ -26,9 +29,23 @@ def extractData(rawData):
             for interval in dateData['intervals']:
                 if interval['free']:
                     freeIntervals.append(interval['formattedDate'])
-            date2freeIntervals[dateData['date']] = freeIntervals
+            date2freeIntervals[unicode(dateData['date'])] = freeIntervals
     return date2freeIntervals
 
+def loadPrevData():
+    if not exists(PREV_DATA_FNAME):
+        open(PREV_DATA_FNAME, 'w').close()
+        prevData = None
+    else:
+        f = open(PREV_DATA_FNAME, "r")
+        prevData = json.loads(f.read())
+        f.close()
+    return prevData
+
+def saveData(data):
+    f = open(PREV_DATA_FNAME, "w")
+    f.write(json.dumps(data, sort_keys=True, indent=2, separators=(',', ': ')))
+    f.close()
 
 def examine():
     # Request data
@@ -38,9 +55,18 @@ def examine():
         return
     rawData = req.json()
     #log(json.dumps(rawData, sort_keys=True, indent=2, separators=(',', ': ')))
+
+    prevData = loadPrevData()
+    if prevData:
+        log("prev data:")
+        log(prevData)
+
     # Getting free intervals
     date2freeIntervals = extractData(rawData)
     log(date2freeIntervals)
+
+    # Save data
+    saveData(date2freeIntervals)
 
 
 if __name__ == '__main__':
