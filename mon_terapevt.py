294dcd52a45ec162dda07f77859e84cb176bdfd8
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-10-03 11:33:57 +0300

    Impl-ed the examination every N sec.

--- mon_terapevt.py
+++ mon_terapevt.py
@@ -1,2 +1,18 @@
 #! python2
 
+import time
+
+#======================================
+# Constants
+
+MON_INTERVAL = 5 * 60 # in sec
+
+#======================================
+
+def examine():
+    print 'examine'
+
+if __name__ == '__main__':
+    while True:
+        examine()
+        time.sleep(MON_INTERVAL)
